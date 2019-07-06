f2b89b22dee630d803656d5794228aa841ddcc90
Parveen Dala <dparveen94@gmail.com>
2019-07-06 20:39:21 +0530

    Stack using array

---
+++ src/stack/Client.java
@@ -0,0 +1,23 @@
+package stack;
+
+public class Client {
+    public static void main(String[] args) {
+        StackUsingArray stack = new StackUsingArray(5);
+        stack.push(5);
+        stack.push(10);
+        stack.push(15);
+        stack.push(20);
+        stack.push(25);
+        stack.push(30);
+        System.out.println("Peeked " + stack.peek());
+        System.out.println("Popped " + stack.pop());
+        System.out.println("Popped " + stack.pop());
+        System.out.println("Popped " + stack.pop());
+        System.out.println("Popped " + stack.pop());
+        System.out.println("Popped " + stack.pop());
+        System.out.println("Popped " + stack.pop());
+        System.out.println("Popped " + stack.pop());
+
+
+    }
+}
---
+++ src/stack/StackUsingArray.java
@@ -0,0 +1,52 @@
+package stack;
+
+/******
+ * Parveen Dala
+ * Stack using Array, Data Structure
+ */
+public class StackUsingArray {
+    int[] stack;
+    int top;
+
+    public StackUsingArray(int size) {
+        stack = new int[size];
+        top = -1;
+    }
+
+    public boolean push(int value) {
+        if (isFull()) {
+            System.out.println("Stack Overflow, Unable to push " + value);
+            return false;
+        } else {
+            stack[++top] = value;
+            System.out.println("Pushed " + value);
+            return true;
+        }
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow, Unable to pop item");
+            return 0;
+        } else {
+            return stack[top--];
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow, Unable to peek item");
+            return 0;
+        } else {
+            return stack[top];
+        }
+    }
+
+    public boolean isEmpty() {
+        return top < 0;
+    }
+
+    public boolean isFull() {
+        return top == stack.length - 1;
+    }
+}
